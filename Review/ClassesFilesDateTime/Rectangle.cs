50f3415b319de063d05c10a803ff222f1880ab1f
Chase Bonar <Chase_Bonar@DESKTOP-0R99T6T>
2020-08-31 12:06:53 -0500

    Complete partial of rectangle

---
+++ Review/ClassesFilesDateTime/Rectangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesFilesDateTime
+{
+    public class Rectangle
+    { 
+    
+        private int width { get; set; }
+        private int height { get; set; }
+
+
+        public DateTime creationDate { get; set; }
+
+             
+
+        public Rectangle()
+        {
+            width = 0;
+            height = 0;
+            creationDate = DateTime.Now;
+
+        }
+        /// <summary>
+        /// Overloaded constructor for the Rectangle Class
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            creationDate = DateTime.Now;
+
+        }
+
+        /// <summary>
+        /// Calculate area of the rectangle
+        /// </summary>
+        /// <returns>The area of the rectangle</returns>
+        public double area()
+        {
+            return width * height;
+        }
+
+
+    }
+}
